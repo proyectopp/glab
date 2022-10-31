ed7f1bd16193e9c61e633d02f5a9909f1cd7d4dd
proyectopp <proyectopp990@gmail.com>
2022-10-31 10:09:06 -0300

    creacionproyecto

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig : {
+    apiKey: "AIzaSyBLgvX_07_xbjwjjo8wATKV3Ukh84Gd9sU",
+    authDomain: "practicas-20c69.firebaseapp.com",
+    projectId: "practicas-20c69",
+    storageBucket: "practicas-20c69.appspot.com",
+    messagingSenderId: "1003145603532",
+    appId: "1:1003145603532:web:ca4f00f0d0284f8becd569"
+  }
 };
 
 /*
